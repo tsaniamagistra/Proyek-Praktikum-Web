54450889c7b960fe190d066c66bdc512f5516f46
tsaniamagistra <123210090@student.upnyk.ac.id>
2022-11-21 15:46:59 +0700

    membuat laman home

---
+++ home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Beranda</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+	<link rel="stylesheet" href="home.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#063970">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+	<div id="top-wrapper">
+		<h1>Selamat Datang!</h1>
+	</div>
+	<div class="d-flex align-items-center justify-content-center" style="height:35vh;">
+		<div class="row" style="width: 50%; text-align: center;">
+			<div class="d-grid col-6">
+				<button type="button" class="btn btn-dark" style="background-color:#063970;">Jadwal Dokter</button>
+			</div>
+			<div class="d-grid col-6">
+				<button type="button" class="btn btn-dark" style="background-color:#063970;">Pendaftaran</button>
+			</div>
+		</div>
+	</div>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
