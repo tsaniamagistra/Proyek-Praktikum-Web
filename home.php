99255672fc2bc9281e932e390c8498f7ff972666
tsaniamagistra <123210090@student.upnyk.ac.id>
2022-11-22 20:45:25 +0700

    mengubah tampilan home dan navbar

--- home.php
+++ home.php
@@ -6,39 +6,49 @@
 	<title>Beranda</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 	<link rel="stylesheet" href="home.css">
+	<style>
+		.list-group-item{
+			border-bottom: 0;
+		}
+		.btn-home{
+			background-color:#063970;
+		}
+	</style>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#063970">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+	<nav class="navbar navbar-dark" style="background-color:#063970">
+  	<div class="container-fluid">
+  	  <a class="navbar-brand"><img src="Images/logo.png" style="height:30px" alt="HOSPITAL"></a>
+  	  <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+  	  	<span class="navbar-toggler-icon"></span>
+  	  </button>
+  	</div>
+	</nav>
+	<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+		<div class="offcanvas-header">
+			<h5 class="offcanvas-title" id="offcanvasRightLabel">Contoh Nama, Nn.</h5>
+			<button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+		</div>
+		<div class="offcanvas-body">
+			<div class="list-group list-group-flush">
+				<a href="home.php" class="list-group-item list-group-item-action" style="font-weight:bold;">Beranda</a>
+				<a href="#" class="list-group-item list-group-item-action">Jadwal Dokter</a>
+				<a href="#" class="list-group-item list-group-item-action">Pendaftaran Periksa</a>
+				<a href="#" class="list-group-item list-group-item-action">Riwayat</a>
+				<a href="#" class="list-group-item list-group-item-action">Logout</a>
+			</div>
+		</div>
+	</div>
 	<div id="top-wrapper">
 		<h1>Selamat Datang!</h1>
 	</div>
 	<div class="d-flex align-items-center justify-content-center" style="height:35vh;">
 		<div class="row" style="width: 50%; text-align: center;">
 			<div class="d-grid col-6">
-				<button type="button" class="btn btn-dark" style="background-color:#063970;">Jadwal Dokter</button>
+				<button type="button" class="btn btn-dark btn-lg btn-home">Jadwal Dokter</button>
 			</div>
 			<div class="d-grid col-6">
-				<button type="button" class="btn btn-dark" style="background-color:#063970;">Pendaftaran</button>
+				<button type="button" class="btn btn-dark btn-lg btn-home">Pendaftaran Periksa</button>
 			</div>
 		</div>
 	</div>
