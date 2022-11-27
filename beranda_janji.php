b03aa14f6bf0d636bd9bc0e99e6777104ec0661d
indahnurrohmah <91123466+indahnurrohmah@users.noreply.github.com>
2022-11-27 18:47:28 +0700

    menambah menu edit di navbar

--- beranda_janji.php
+++ beranda_janji.php
@@ -64,6 +64,7 @@
 				<a href="beranda_janji.php" class="list-group-item list-group-item-action" style="font-weight:bold;">Buat Janji Dokter</a>
 				<a href="riwayat.php" class="list-group-item list-group-item-action">Riwayat</a>
 				<?php if(!empty($_SESSION['no_rm'])){?>
+					<a href="edit_biodata.php" class="list-group-item list-group-item-action">Edit Biodata</a>
 					<a href="logout.php" class="list-group-item list-group-item-action"><?="Keluar";
 					?></a>
 				<?php }?>
